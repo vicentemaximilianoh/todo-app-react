82b8744ad35b912b815d0012cc039653b046d931
Maximiliano Vicente <vicente.maximilianoh@gmail.com>
2021-02-25 11:31:58 -0300

    Add TodoList component with hardcoded list

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+
+import TodoList from './components/TodoList/TodoList';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <TodoList></TodoList>
       </header>
     </div>
   );
---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function TodoList() {
+    return (
+        <div className="TodoList">
+            <div className="TodoList-title">Todo List</div>
+            <ul className="TodoList-items">
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 2</li>
+                <li>Item 4</li>
+            </ul>
+        </div>
+    )
+}
+
+export default TodoList;
