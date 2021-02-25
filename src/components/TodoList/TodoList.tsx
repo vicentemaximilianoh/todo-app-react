587a5380edace523b84c4513d0b77200f7566aa6
Maximiliano Vicente <vicente.maximilianoh@gmail.com>
2021-02-25 14:08:37 -0300

    Added 'Add item to list' feature. Items are not hardcoded anymore

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, {FormEvent, useState} from 'react';
 
 function TodoList() {
+    const initialItems: string[] = [];
+
+    const [newItem, setNewItem]: [string, React.Dispatch<React.SetStateAction<string>>] = useState('');
+    const [items, setItems]: [string[], React.Dispatch<React.SetStateAction<string[]>>] = useState(initialItems);
+
+    const onSubmit = function(e: Event) {
+        e.preventDefault();
+        setItems(items.concat(newItem));
+        setNewItem('');
+    }
+
+    let listItems: any[] = [];
+    items.map((item, index) => {
+        listItems.push(<li key={index}>{item}</li>);
+    })
+
     return (
         <div className="TodoList">
             <div className="TodoList-title">Todo List</div>
+            <form 
+                className="TodoList-add"
+                onSubmit={(e: any) => onSubmit(e)}>
+                <input 
+                    onChange={(e) => setNewItem(e.target.value)}
+                    value={newItem}/>
+            </form>
             <ul className="TodoList-items">
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 2</li>
-                <li>Item 4</li>
+                {listItems}
             </ul>
         </div>
     )
